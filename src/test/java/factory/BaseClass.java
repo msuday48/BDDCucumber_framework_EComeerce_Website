5ac51970e02ca3d03e953fea7a2c80add054c191
msuday48 <msuday48@gmail.com>
2025-07-02 10:36:59 +0530

    push code

--- src/test/java/factory/BaseClass.java
+++ src/test/java/factory/BaseClass.java
@@ -1,11 +1,11 @@
 package factory;
 
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
-
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,7 +18,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class
 BaseClass {
-
 		 static WebDriver driver;
 	     static Properties p;
 	     static Logger logger;
@@ -94,19 +93,15 @@ BaseClass {
 		return logger;
 	}
 	
-	public static String randomeString()
-	{
-		String generatedString=RandomStringUtils.randomAlphabetic(5);
+	public static String randomeString() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
 		return generatedString;
 	}
-	
-	
 	public static String randomeNumber()
 	{
 		String generatedString=RandomStringUtils.randomNumeric(10);
 		return generatedString;
 	}
-
 	public static String randomAlphaNumeric()
 	{
 	String str=RandomStringUtils.randomAlphabetic(5);
